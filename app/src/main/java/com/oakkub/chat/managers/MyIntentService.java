b4560144c5564e337d4a7afc372e9ebe0cf2bdc6
OaKKuB <oakkub.1995@gmail.com>
2016-03-19 11:54:56 +0700

    beta released

--- app/src/main/java/com/oakkub/chat/managers/MyIntentService.java
+++ app/src/main/java/com/oakkub/chat/managers/MyIntentService.java
@@ -1,7 +1,26 @@
 package com.oakkub.chat.managers;
 
+import android.app.IntentService;
+import android.os.Handler;
+
 /**
  * Created by OaKKuB on 3/16/2016.
  */
-public class MyIntentService {
+public abstract class MyIntentService extends IntentService {
+
+    private Handler handler;
+
+    public MyIntentService(String name) {
+        super(name);
+    }
+
+    @Override
+    public void onCreate() {
+        handler = new Handler();
+        super.onCreate();
+    }
+
+    protected void runOnUiThread(Runnable runnable) {
+        handler.post(runnable);
+    }
 }
